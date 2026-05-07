ac0e7cd69bc9ef7677d0e8dbacdfc5850812f3a3
Deep Desai <deepdesai@mac.mynetworksettings.com>
2026-05-06 19:07:50 -0500

    Initial commit - Hello World app

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello from AWS CI/CD Pipeline! 🚀</h1>');
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});s
+
